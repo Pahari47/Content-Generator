c76548592f327bd9fc92a8d1dcfaf5ce3ddb469f
Pahari47 <nachiketapahari00@gmail.com>
2024-07-13 12:51:34 +0530

    frontend and api complete

--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -1,23 +1,29 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { TEMPLATE } from '../../_components/TemplateList';
 import Image from 'next/image';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { Loader2Icon } from 'lucide-react';
 
 interface PROPS{
     selectedTemplate?:TEMPLATE;
+    userFormInput:any,
+    loading:boolean
 }
 
-function FormSection({selectedTemplate}:PROPS) {
+function FormSection({selectedTemplate, userFormInput, loading}:PROPS) {
+  const [formData, setFormData] = useState<any>();
 
   const handleInputChange=(event:any)=>{
-
+    const {name, value} = event.target;
+    setFormData({...formData, [name]:value})
   }
 
     const onSubmit = (e:any)=>{
         e.preventDefault()
+        userFormInput(formData)
     }
   return (
     <div className='p-5 shadow-md border rounded-lg bg-white'>
@@ -33,7 +39,9 @@ function FormSection({selectedTemplate}:PROPS) {
                 {item.field=='input'? <Input name={item.name} required={item?.required} onChange={handleInputChange} /> :item.field=='textarea'? <Textarea name={item.name} required={item?.required} onChange={handleInputChange} />: null}
             </div>
         ))}
-        <Button type='submit' className='w-full py-6'>Generate</Button>
+        <Button type='submit' className='w-full py-6' disabled={loading} >
+          {loading&&<Loader2Icon className='animate-spin text-white' />}
+          Generate</Button>
       </form>
     </div>
   )
